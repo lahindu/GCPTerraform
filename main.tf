9b0c39b035be5e1996de85c2d42d859010c86a35
Lahindu Weerarathna <Lahindu_105097@dialog.dialoggsm.com>
2020-10-16 13:12:31 +0530

    Initial commit

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "vpc" {
     vpc_description             = "EKS Development Environment"
     subnet_cidr_blocks          = [ "10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24", "10.1.4.0/24" ]
     subnet_name                 = [ "vpc-gke-1-web", "vpc-gke-1-dtesvc", "vpc-gke-1-db", "vpc-gke-1-toolstack" ]
-    region                      = [ "us-east1", "us-east1", "us-east1", "us-east2" ]
+    region                      = [ "us-east1", "us-east1", "us-east1", "us-east4" ]
     cloud_router                = "vpc-gke-1-cldrt"
     cloud_router_region         = "us-east1"
     nat_name                    = "vpc-gke-1-cldnat"
