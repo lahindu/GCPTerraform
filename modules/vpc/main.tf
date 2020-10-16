f83db2ef37ec1f6bfb1470827054fbdadf94157e
Lahindu Weerarathna <Lahindu_105097@dialog.dialoggsm.com>
2020-10-16 23:22:58 +0530

    Initial commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,15 +16,19 @@ resource "google_compute_subnetwork" "subnet" {
 }
 
 resource "google_compute_router" "cloud_router" {
-    name    = var.cloud_router
-    region  = var.cloud_router_region
-    network = google_compute_network.vpc.name
+    name                = var.cloud_router
+    region              = var.cloud_router_region
+    network             = google_compute_network.vpc.name
+    dest_range          = "0.0.0.0/0"
+    priority            = 100
+    next_hop_gateway    = "default-internet-gateway"
+
 }
 
-resource "google_compute_router_nat" "nat" {
-    name                               = var.nat_name
-    router                             = google_compute_router.cloud_router.name
-    region                             = google_compute_router.cloud_router.region
-    nat_ip_allocate_option             = "AUTO_ONLY"
-    source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-}
+#resource "google_compute_router_nat" "nat" {
+#    name                               = var.nat_name
+#    router                             = google_compute_router.cloud_router.name
+#    region                             = google_compute_router.cloud_router.region
+#    nat_ip_allocate_option             = "AUTO_ONLY"
+#    source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+#}
